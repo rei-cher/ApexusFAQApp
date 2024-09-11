6d962c34fc801069c1542d30632453679750da11
Pavel Drozdov <p4shage@gmail.com>
2024-09-11 19:17:42 -0400

    Splash screens and app icon

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import NavBar from './src/components/NavBar';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    // Splash screen for 3 seconds
+    const timeout = setTimeout(() => {
+      SplashScreen.hide();
+    }, 3000);
+
+    // Cleanup the timeout
+    return () => clearTimeout(timeout);
+  }, [])
+
   return (
     <NavigationContainer>
       <NavBar />
